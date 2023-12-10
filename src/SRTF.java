0a3bc15d39f07fa346721ce6cd7587c3c43efe5c
Samaselim1 <125610375+Samaselim1@users.noreply.github.com>
2023-12-10 20:47:28 +0200

    SRTF update

--- src/SRTF.java
+++ src/SRTF.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class SRTF implements Ischeduler {
     private List<Process> processes;
+    private static final double WaitTime_threshold = 10; // Maximum waiting time threshold
 
     public void setProcesses(List<Process> processes) {
         this.processes = processes;
@@ -20,7 +21,8 @@ public class SRTF implements Ischeduler {
 
             for (int i = 0; i < processes.size(); i++) {
                 Process process = processes.get(i);
-                if (!executedProcesses.contains(process) && process.getArrivalTime() <= currentTime && process.getBurstTime() < minBurst) {
+                if (!executedProcesses.contains(process) && process.getArrivalTime() <= currentTime
+                        && process.getBurstTime() < minBurst) {
                     shortestIndex = i;
                     minBurst = process.getBurstTime();
                 }
@@ -32,25 +34,37 @@ public class SRTF implements Ischeduler {
             }
 
             Process shortest = processes.get(shortestIndex);
+
+            // Check and handle processes exceeding the wait time threshold
+            for (Process process : processes) {
+                if (!executedProcesses.contains(process) && process != shortest
+                        && process.getArrivalTime() <= currentTime) {
+                    process.setWaitTime(process.getWaitTime() + 1); // Increment wait time for waiting processes
+
+                    if (process.getWaitTime() >= WaitTime_threshold) {
+                        // If wait time exceeds or equals the threshold, push the process to the front
+                        processes.remove(process);
+                        processes.add(0, process);
+                        System.out.println("****Pushing Process to Front : " + process.getName() + " at time " + currentTime);
+
+                        // Preempt the current process and execute the pushed process immediately
+                        if (shortest.getBurstTime() > 0) {
+                            System.out.println("****Preempting Process: " + shortest.getName() + " at time " + currentTime);
+                            Process preemptedProcess = shortest; // Temporarily store the current process
+                            processes.remove(preemptedProcess);
+                            processes.add(0, process); // Move the pushed process to the front
+                            processes.add(process); // Add the preempted process back to the end of the queue
+                            shortestIndex = 0;
+                            break; // Exit the loop to execute the pushed process immediately
+                        }
+                    }
+                }
+            }
             double startTime = currentTime;
             System.out.println("Executing Process: " + shortest.getName() + " from time " + startTime);
 
             shortest.setBurstTime(shortest.getBurstTime() - 1);
             currentTime++;
-
-            // Aging: Decrement priority for waiting processes
-            for (Process process : processes) {
-                if (!executedProcesses.contains(process) && process != shortest) {
-                    int priority = process.getPriority();
-                    if (currentTime - process.getArrivalTime() > 0) {
-                        if (priority < 1) {
-                            process.setPriority(1);
-                        } else {
-                            process.setPriority(priority);
-                        }
-                    }
-                }
-            }
 
             if (shortest.getBurstTime() == 0) {
                 executedProcesses.add(shortest);
@@ -68,14 +82,6 @@ public class SRTF implements Ischeduler {
                 System.out.println("Waiting Time for Process " + shortest.getName() + ": " + waitingTime);
                 System.out.println("Turnaround Time for Process " + shortest.getName() + ": " + turnaroundTime);
                 System.out.println("--------------------------------");
-            } else {
-                for (Process process : processes) {
-                    if (!executedProcesses.contains(process) && process != shortest &&
-                            process.getArrivalTime() <= currentTime && process.getBurstTime() < shortest.getBurstTime()) {
-                        System.out.println("****Preempting Process: " + shortest.getName() + " at time " + currentTime);
-                        break;
-                    }
-                }
             }
         }
 
