ba8156a96bbf4e1bbb36212592ea22670a75106d
rawanntarek <rawaaan245@gmail.com>
2023-12-12 22:23:08 +0200

    zft

--- src/AG.java
+++ src/AG.java
@@ -4,8 +4,6 @@ public class AG implements Ischeduler {
     private List<Process> processes;
     private double quantum;
     private boolean isPreemptive = false;
-    private int starttime;
-    private int finishtime;
     private int currentTime = 0;
 
     public AG(int quantum) {
@@ -42,88 +40,99 @@ public class AG implements Ischeduler {
 
     @Override
     public void schedule() {
-        List<Process> readyQueue = new ArrayList<>();
-        Iterator<Process> iterator = processes.iterator();
-
-        while (iterator.hasNext()) {
-            Process process = iterator.next();
-            if (process.getArrivalTime() <= currentTime) {
-                readyQueue.add(process);
-                iterator.remove(); // Remove safely using iterator
-            }
-        }
-
-
-        if(!readyQueue.isEmpty() ) {
-            Process currentExecution = readyQueue.get(0);
-            double remaining = currentExecution.getBurstTime()-quantum;
-            double limit= Math.ceil(0.5*quantum);
-            if(remaining>limit)
-            {
-                currentExecution.setBurstTime(remaining);
-                currentTime+=limit;
-                if (!isPreemptive && currentTime - currentExecution.getstarttime() >= limit) {
-                    isPreemptive=false;
-                }
-
-
-            }
-            else
-            {
-                currentExecution.setBurstTime(0);
-                currentExecution.setPreemptive(true);
-                if (!isPreemptive && currentTime - currentExecution.getstarttime() >= limit) {
-                    isPreemptive = true;
-                }
-                currentExecution.setEndtime(currentTime);
-
-            }
-        }
-        if(isPreemptive && !readyQueue.isEmpty())
-        {
-            Process currentExecution = readyQueue.get(0);
-            for(Process p:readyQueue)
-            {
-                double agFactorCurrent = AGFactor(currentExecution);
-                double agFactorNew = AGFactor(p);
-                if (agFactorNew < agFactorCurrent || (agFactorNew == agFactorCurrent && p.getArrivalTime() < currentExecution.getArrivalTime())) {
-                    readyQueue.add(currentExecution);
-                    readyQueue.remove(p);
-                    currentExecution = p;
+        Queue<Process> readyQueue = new LinkedList<>();
+        Queue<Process> executedProcesses = new LinkedList<>();
+        double totalWaitingTime = 0;
+        double totalTurnaroundTime = 0;
+        while (!processes.isEmpty() || !readyQueue.isEmpty()) {
+            // Add arrived processes to the ready queue
+            for (Process process : new ArrayList<>(processes)) {
+                if (process.getArrivalTime() <= currentTime) {
+                    readyQueue.add(process);
+                    processes.remove(process);
                 }
             }
-            //scenario 1
-            if(currentExecution.getBurstTime()>0)
-            {
-                double limit=Math.ceil(0.1*quantum);
-                readyQueue.add(currentExecution);
-                currentExecution.setBurstTime(0);
-                currentExecution.setEndtime(currentTime);
-                isPreemptive=true;
-                currentExecution.setQuantum(currentExecution.getQuantum()+limit);
+
+            if (!readyQueue.isEmpty()) {
+                Process currentExecution = readyQueue.poll();
+                double remaining = currentExecution.getBurstTime() - quantum;
+                double limit = Math.ceil(0.5 * quantum);
+
+                // Scenario iii: The running process finished its job
+                if (remaining <= 0) {
+                    currentExecution.setBurstTime(0);
+                    currentExecution.setPreemptive(true);
+                    if (!isPreemptive && currentTime - currentExecution.getstarttime() >= limit) {
+                        isPreemptive = true;
+                    }
+                    currentExecution.setEndtime(currentTime);
+                    executedProcesses.add(currentExecution);
+
+                } else {
+                    // Scenario i: The running process used all its quantum time
+                    if (remaining > limit) {
+                        currentExecution.setBurstTime(remaining);
+                        currentTime += limit;
+                        if (!isPreemptive && currentTime - currentExecution.getstarttime() >= limit) {
+                            isPreemptive = false;
+                        }
+                        quantum += Math.ceil(0.1 * (quantum / 2)); // Increase Quantum time
+                        readyQueue.add(currentExecution);
+                    }
+                    // Scenario ii: The running process didn’t use all its quantum time
+                    else {
+                        currentExecution.setBurstTime(0);
+                        currentExecution.setPreemptive(true);
+                        if (!isPreemptive && currentTime - currentExecution.getstarttime() >= limit) {
+                            isPreemptive = true;
+                        }
+                        currentExecution.setEndtime(currentTime);
+                        executedProcesses.add(currentExecution);
+                    }
+                }
             }
-            //scenario 2
-            if(currentExecution.getBurstTime()>0)
-            {
-                double remaining=currentExecution.getBurstTime()-quantum;
-                readyQueue.add(currentExecution);
-                currentExecution.setBurstTime(0);
-                currentExecution.setEndtime(currentTime);
-                isPreemptive=true;
-                currentExecution.setQuantum(currentExecution.getQuantum()+remaining);
+
+            // Preemptive AG: Reorder the ready queue based on AGFactor
+            if (isPreemptive && !readyQueue.isEmpty()) {
+                List<Process> sortedList = new ArrayList<>(readyQueue);
+                sortedList.sort(Comparator.comparingDouble(this::AGFactor));
             }
-            if(currentExecution.getBurstTime()==0)
-            {
-                readyQueue.remove(currentExecution);
-                currentExecution.setEndtime(currentTime);
-                currentExecution.setQuantum(0);
+
+            // Print history of quantum time
+            System.out.println("AG Scheduling: Quantum time updated for each process");
+            for (Process process : readyQueue) {
+                System.out.println(process.getName() + ": " + quantum);
             }
-            currentExecution.setStarttime(currentTime);
+            currentTime += quantum; // Update the current time
         }
 
+        // Print execution order
+        System.out.print("Processes execution order: ");
+        for (Process process : executedProcesses) {
+            System.out.print(process.getName() + " ");
+        }
+        System.out.println();
+
+        // Print Waiting Time for each process
+        System.out.println("Waiting Time for each process:");
+        for (Process process : executedProcesses) {
+            double waiting = process.getstarttime() - process.getArrivalTime();
+            System.out.println(process.getName() + ": " + waiting);
+            totalWaitingTime += waiting;
+        }
+
+        // Print Turnaround Time for each process
+        System.out.println("Turnaround Time for each process:");
+        for (Process process : executedProcesses) {
+            double turnaround = process.getEndtime() - process.getArrivalTime();
+            System.out.println(process.getName() + ": " + turnaround);
+            totalTurnaroundTime += turnaround;
+        }
+
+        // Print Average Waiting Time and Average Turnaround Time
+        double averageWaitingTime = totalWaitingTime / executedProcesses.size();
+        double averageTurnaroundTime = totalTurnaroundTime / executedProcesses.size();
+        System.out.println("Average Waiting Time: " + averageWaitingTime);
+        System.out.println("Average Turnaround Time: " + averageTurnaroundTime);
     }
-
-
-
-
 }
